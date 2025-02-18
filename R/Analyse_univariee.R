cb194c2355e71de9de3476d33961724de6e6e883
Timothe Fadenipo <timothe.fadenipo@etudiant.univ-reims.fr>
2025-02-18 20:34:48 +0100

    1

--- R/Analyse_univariee.R
+++ R/Analyse_univariee.R
@@ -1,6 +1,6 @@
 #Analyse univariée ----
 
-# Installer et charger le package corrplot
+# Installer et charger les package
 install.packages("readxl")
 install.packages("corrplot")
 library(readxl)
@@ -11,3 +11,9 @@ library(corrplot)
 
 # Charger les données ( Verifier si on se trouve bien dans le repertoire juste avant "Data")
 bdd <- read_excel("Data/Concrete_Data.xls")
+
+
+head(bdd,5)
+colnames(bdd)
+
+summary(bdd$`Concrete compressive strength(MPa, megapascals)`)
